7b6a0f8109125cd709a4d316f7ad055d7980e3fc
Jhostym <jhosrohe27@gmail.com>
2023-03-03 20:44:59 -0500

    creando vista usuarios

--- src/pages/Usuario/index.jsx
+++ src/pages/Usuario/index.jsx
@@ -1,104 +1,268 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
-import "./Usuario.css";
-
-
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import './Usuario.css';
 
 const UserPage = () => {
-
     const { user } = useContext(AuthContext);
 
-
-    const [account, setAccount] = useState({
-        name: "",
-    });
-
+    const [dataUser, setDataUser] = useState([]);
+    const [reserPasad, setReserPasad] = useState([]);
+    const [reserFut, setReserFut] = useState([]);
+    const [showTable, setShowTable] = useState(true);
+    const [showTableFut, setShowTableFut] = useState(false);
 
     // función para hacer la petición GET
     const getUser = async (setAccount) => {
         try {
-          const token = user.token;
-          const id = user._id;
-          const response = await fetch(`http://localhost:5000/api/v1/auth/user/${id}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
-          const data = await response.json();
-      
-          setAccount(data);
+            const token = user.token;
+            const id = user._id;
+            const response = await fetch(
+                `http://localhost:5000/api/v1/auth/user/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            const data = await response.json();
+
+            setAccount(data);
+
+            console.log(data)
+
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      };
+    };
     // llamas a la función para hacer la petición GET
-    
+
     useEffect(() => {
-        getUser(setAccount);
-      }, []);
+
+        obtenerDatos();
+        obtenerReservasPasadas();
+        obtenerReservasFuturas();
+    }, []);
+
+    const obtenerDatos = async () => {
+        const token = user.token;
+        const id = user._id;
+        const response = await fetch(
+            `http://localhost:5000/api/v1/auth/user/${id}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const data = await response.json();
+        setDataUser(data.user);
+    };
+
+    const obtenerReservasPasadas = async () => {
+        const token = user.token;
+        const response = await fetch(
+            'http://localhost:5000/api/v1/historial/past/',
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const data = await response.json();
+        const dataFinal = data?.reservasPasadas;
+        setReserPasad(dataFinal);
+    };
+
+    const obtenerReservasFuturas = async () => {
+        const token = user.token;
+        const response = await fetch(
+            'http://localhost:5000/api/v1/historial/futuro/',
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const data = await response.json();
+        const dataFinal = data?.reservasFuturas;
+        console.log(dataFinal);
+        setReserFut(dataFinal);
+    };
+    const handleClick = () => {
+        setShowTable(true);
+        setShowTableFut(false);
+    };
+
+    const handleClickFut = () => {
+        setShowTable(false);
+        setShowTableFut(true);
 
 
-    return (
-        <section className="bg-light">
-            <div className="container">
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-10 mt-5 pt-5">
-                        <div className="row z-depth-3">
-                            <div className="col-sm-4 bg-info rounded-left">
-
-                                <div className="card-block text-center text-white">
-                                    <i className="fas fa-user-tie fa-7x mt-5"></i>
+    }; return (
+        <section className='bg-light'>
+            <div className='container'>
+                <div className='row d-flex justify-content-center'>
+                    <div className='col-md-10 mt-5 pt-5'>
+                        <div className='row'>
+                            <div className='col-sm-4 bg-info rounded-left'>
+                                <div className='card-block text-center text-white'>
+                                    <i className='fas fa-user-tie fa-7x mt-5'></i>
 
                                     <p>Usuario</p>
-                                    <i className="far fa-edit fa-2x mb-4"></i>
+                                    <i className='far fa-edit fa-2x mb-4'></i>
                                 </div>
                             </div>
-                            <div className="col-sm-8 bg-white rounded-right">
-                                <h3 className="mt-3 text-center">Datos Personales</h3>
-                                <hr className="badge-primary mt-0 w-35" />
-                                <div className="row align-items-start">
-                                    <div className="col">
-                                        <p className="font-weight-bold">Nombre:</p>
-                                        <h6 className="text-muted">{account.name}</h6>
+                            <div className='col-sm-8 bg-white rounded-right'>
+                                <h3 className='mt-3 text-center'>Datos Personales</h3>
+                                <hr className='badge-primary mt-0 w-35' />
+                                <div className='row align-items-start'>
+                                    <div className='col'>
+                                        <p className='font-weight-bold'>Nombre:</p>
+                                        <h6 className='text-muted'>{dataUser?.name}</h6>
                                     </div>
-                                    <div className="col">
-                                        <p className="font-weight-bold">Apellidos:</p>
-                                        <h6 className="text-muted">Senior</h6>
+                                    <div className='col'>
+                                        <p className='font-weight-bold'>Apellidos:</p>
+                                        <h6 className='text-muted'>Senior</h6>
                                     </div>
-                                    <div className="col">
-                                        <p className="font-weight-bold">celular:</p>
-                                        <h6 className="text-muted">962458675</h6>
+                                    <div className='col'>
+                                        <p className='font-weight-bold'>celular:</p>
+                                        <h6 className='text-muted'>962458675</h6>
                                     </div>
 
-                                    <div className="row align-items-end"></div>
+                                    <div className='row align-items-end'></div>
 
-                                    <div className="col">
-                                        <p className="font-weight-bold">Usuario:</p>
-                                        <h6 className="text-muted">Nixon@gmail.com</h6>
+                                    <div className='col'>
+                                        <p className='font-weight-bold'>Usuario:</p>
+                                        <h6 className='text-muted'>Nixon@gmail.com</h6>
                                     </div>
                                 </div>
-
-
                             </div>
-                            <h3 className="mt-3">Historial</h3>
-                            <div className="row">
-
+                            <h3 className='mt-3'>Historial</h3>
+                            <div className='row'>
                                 <div>
-                                    <hr className="bg-primary" />
-                                    <ul className="list-unstyled d-flex justify-content-start mt-4">
-                                        <button type="button" className="btn btn-success btn-lg">Reservas Pasadas</button>
-                                        <button type="button" className="btn btn-warning btn.lg">Reservas Pendientes</button>
+                                    <hr className='bg-primary' />
+                                    <ul className='list-unstyled d-flex justify-content-start mt-4'>
+                                        <button
+                                            type='button'
+                                            className='btn btn-success btn-lg'
+                                            onClick={handleClick}
+                                        >
+                                            Reservas Pasadas
+                                        </button>
+                                        <button
+                                            type='button'
+                                            className='btn btn-warning btn.lg'
+                                            onClick={handleClickFut}
+                                        >
+                                            Reservas Futuras
+                                        </button>
                                     </ul>
                                 </div>
                             </div>
-
+                            {/* Table Begins */}
+                            <div>
+                                {showTable && (
+                                    <>
+                                        <table className='table table-striped'>
+                                            <thead>
+                                                <tr>
+                                                    <th scope='col'>#</th>
+                                                    <th scope='col'>Cancha</th>
+                                                    <th scope='col'>Fecha de la Reserva</th>
+                                                    <th scope='col'>Hora Inicio</th>
+                                                    <th scope='col'>Hora Fin</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {reserPasad.map((val, index) => (
+                                                    <tr key={index}>
+                                                        <th scope='row'>{index + 1}</th>
+                                                        <td>Cancha</td>
+                                                        <td>
+                                                            {new Date(val?.fecha_reserva).toLocaleDateString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                        <td>
+                                                            {new Date(val?.hora_inicio).toLocaleTimeString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                        <td>
+                                                            {new Date(val?.hora_fin).toLocaleTimeString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </>
+                                )}
+                                {showTableFut && (
+                                    <>
+                                        <table class='table table-striped'>
+                                            <thead>
+                                                <tr>
+                                                    <th scope='col'>#</th>
+                                                    <th scope='col'>Cancha</th>
+                                                    <th scope='col'>Fecha de la Reserva</th>
+                                                    <th scope='col'>Hora Inicio</th>
+                                                    <th scope='col'>Hora Fin</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {reserFut.map((val, index) => (
+                                                    <tr key={index}>
+                                                        <th scope='row'>{index + 1}</th>
+                                                        <td>Cancha</td>
+                                                        <td>
+                                                            {new Date(val?.fecha_reserva).toLocaleDateString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                        <td>
+                                                            {new Date(val?.hora_inicio).toLocaleTimeString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                        <td>
+                                                            {new Date(val?.hora_fin).toLocaleTimeString(
+                                                                'es-ES',
+                                                                {
+                                                                    timeZone: 'UTC',
+                                                                }
+                                                            )}
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     );
-}
+};
 
 export default UserPage;
